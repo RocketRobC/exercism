aea46b207641a71e386c232261c015da04838909
Rob Cornish <rob@robcornishmusic.com>
2016-03-17 20:41:58 +1100

    refactored robot_name

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,27 +1,21 @@
 class Robot
 
+  attr_reader :name
+
   def initialize
-    name
-  end
-
-  def name
-    @current_name ||= set_name
-  end
-
-  def set_name
-    (rand_alpha << rand_num).join
+    @name = rand_alpha << rand_num
   end
 
   def rand_alpha
-    2.times.map { (65 + rand(26)).chr }
+    2.times.map { (65 + rand(26)).chr }.join
   end
 
   def rand_num
-    3.times.map { 1 + rand(9) }
+    3.times.map { 1 + rand(9) }.join
   end
 
   def reset
-    @current_name = Robot.new.name
+    initialize
   end
 
 end
