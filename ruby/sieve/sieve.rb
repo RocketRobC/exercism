a2f1d513f7bf5d4ad387764e6bbdd1482b81fd4b
Rob Cornish <rob.cornish@fluidintel.com>
2016-07-09 00:32:28 +1000

    Sieve V1

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,18 @@
+class Sieve
+
+  def initialize(n)
+    @n = n
+  end
+
+  def primes
+  s = (0..@n).to_a
+  s[0] = s[1] = nil
+  s.each do |v|
+    next unless v
+    break if v*v > @n
+      (v*v).step(@n, v) { |i| s[i] = nil }
+    end
+  s.compact
+  end
+
+end
