d2b08a636d5fda59f7a95cc3163620a897fe63d6
Rob Cornish <rob.cornish@fluidintel.com>
2016-12-18 15:33:13 +1100

    bob v1

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,16 @@
+class Bob
+
+  def hey(remark)
+    case 
+    when remark == remark.upcase && remark != remark.downcase
+      'Whoa, chill out!'
+    when remark.end_with?('?')
+      'Sure.'
+    when remark =~ /\A\s*\z/
+      "Fine. Be that way!"
+    when !remark.match(/^[^A-z]*$/) || remark
+      'Whatever.'
+    end
+  end
+
+end
