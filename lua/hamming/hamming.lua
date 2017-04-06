0df679d19a80a50bf41eccd8480106dce69d2a9a
Rob Cornish <rob.cornish@fluidintel.com>
2017-04-06 15:00:26 +1000

    added lua

---
+++ lua/hamming/hamming.lua
@@ -0,0 +1,12 @@
+module 'hamming'
+
+function compute(str1, str2)
+  local diff_count = 0
+  if #str1 ~= #str2 then return -1 end
+  for i = 1, #str1 do
+    if str1:sub(i,i) ~= str2:sub(i,i) then
+      diff_count = diff_count + 1
+    end
+  end
+  return diff_count
+end
