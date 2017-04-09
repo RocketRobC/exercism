b93d690fed7df77b1182061472862a757485b17e
Rob Cornish <rob.cornish@fluidintel.com>
2017-04-09 23:39:58 +1000

    update to lua house

--- lua/house/house.lua
+++ lua/house/house.lua
@@ -1,9 +1,34 @@
 local house = {}
 
-function house.verse()
+local phrases = {
+  'the house that Jack built.',
+  'the malt\nthat lay in ',
+  'the rat\nthat ate ',
+  'the cat\nthat killed ',
+  'the dog\nthat worried ',
+  'the cow with the crumpled horn\nthat tossed ',
+  'the maiden all forlorn\nthat milked ',
+  'the man all tattered and torn\nthat kissed ',
+  'the priest all shaven and shorn\nthat married ',
+  'the rooster that crowed in the morn\nthat woke ',
+  'the farmer sowing his corn\nthat kept ',
+  'the horse and the hound and the horn\nthat belonged to '
+}
+
+function house.verse(recite_from)
+  local verse = ''
+  for i = recite_from, 1, -1 do 
+    verse = verse .. phrases[i]
+  end
+  return 'This is ' .. verse
 end
 
 function house.recite()
+  local song = {}
+  for i = 1, #phrases do
+    table.insert(song, house.verse(i))
+  end
+  return table.concat(song, '\n')
 end
 
 return house
