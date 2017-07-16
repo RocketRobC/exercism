9faf99d9cd0d3c42ebe0c534446d1a85030a08d9
Rob Cornish <rob.cornish@fluidintel.com>
2017-07-16 22:42:22 +1000

    prime_factors update

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -2,7 +2,7 @@ class PrimeFactors
   def self.for(n)
     return [] if n == 1
     factor = (2..n).find { |i| (n % i).zero? }
-    [factor] + self.for(n / factor) if factor
+    [factor] + self.for(n / factor)
   end
 end
 
