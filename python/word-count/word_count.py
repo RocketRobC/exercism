f11bd83bc37e9913ada146f28df76cdfb185d704
Rob Cornish <rob.cornish@fluidintel.com>
2017-08-02 23:33:12 +1000

    python word count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,9 +1,9 @@
 import re
-import string
 
 def word_count(s):
     result = {}
-    words = re.findall('[%s]+' % string.ascii_letters, s)
+    words = re.split('[^a-zA-Z0-9]', s.lower())
     for word in words:
         result[word] = words.count(word)
+    if '' in result: del result['']
     return result
