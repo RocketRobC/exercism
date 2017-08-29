f825fbe57c8637b64e33f89a0a59ebb21a51db86
Rob Cornish <rob.cornish@fluidintel.com>
2017-08-29 20:50:55 +1000

    bob python

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,9 @@
-def hey():
-    pass
+def hey(remark):
+    if remark == remark.upper() and remark != remark.lower():
+        return 'Whoa, chill out!'
+    elif remark.rstrip().endswith('?'):
+        return 'Sure.'
+    elif len(remark.strip()) == 0:
+        return 'Fine. Be that way!'
+    else:
+        return 'Whatever.'
